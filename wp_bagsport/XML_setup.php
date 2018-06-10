c1cd623f7b2cd1ef734c7671e190c0397f70b3eb
Dawid Worhacz <worhacz.dawid@gmail.com>
2018-06-10 20:40:07 +0200

    Ladowanie wszystkich XMLi z produktami

--- wp_bagsport/XML_setup.php
+++ wp_bagsport/XML_setup.php
@@ -3,24 +3,6 @@ require_once __DIR__ . "/php/cfg.php";
 
 $SHOP  = array();
 
-$SHOP['axpol'] = new AXPOL( array(
-	'products' => 'ftp://userPL099:QwqChVFh@ftp.axpol.com.pl/axpol_product_data_PL.xml',
-	'stock' => 'ftp://userPL099:QwqChVFh@ftp.axpol.com.pl/axpol_stocklist_pl.xml',
-	'marking' => 'ftp://userPL099:QwqChVFh@ftp.axpol.com.pl/axpol_print_data_PL.xml',
-	
-),
-array(
-	'shop' => 'AXPOL',
-	
-) );
-$SHOP['easygifts'] = new EASYGIFTS( array(
-	'products' => 'http://www.easygifts.com.pl/data/webapi/pl/xml/offer.xml',
-	'stock' => 'http://www.easygifts.com.pl/data/webapi/pl/xml/stocks.xml',
-	
-),
-array(
-	'shop' => 'EASYGIFTS',
-) );
 $SHOP['macma'] = new MACMA( array(
 	'products' => 'http://www.macma.pl/data/webapi/pl/xml/offer.xml',
 	'stock' => 'http://www.macma.pl/data/webapi/pl/xml/stocks.xml',
@@ -31,6 +13,7 @@ array(
 	'shop' => 'MACMA',
 	
 ) );
+
 $SHOP['inspirion'] = new INSPIRION( array(
 	'products' => 'https://inspirion.pl/sites/default/files/exports/products.xml',
 	
@@ -39,6 +22,36 @@ array(
 	'shop' => 'INSPIRION',
 	
 ) );
+
+$SHOP['anda'] = new ANDA( array(
+	'products' => 'http://andapresent.hu/admin/system/anda_xml_export2.php?&orszag_id=6&nyelv_id=7&password=92ba3632c8c22ebd65fbce872b317875',
+),
+array(
+	'shop' => 'ANDA',
+	
+) );
+
+
+$SHOP['easygifts'] = new EASYGIFTS( array(
+	'products' => 'http://www.easygifts.com.pl/data/webapi/pl/xml/offer.xml',
+	'stock' => 'http://www.easygifts.com.pl/data/webapi/pl/xml/stocks.xml',
+	
+),
+array(
+	'shop' => 'EASYGIFTS',
+) );
+
+$SHOP['axpol'] = new AXPOL( array(
+	'products' => 'ftp://userPL099:QwqChVFh@ftp.axpol.com.pl/axpol_product_data_PL.xml',
+	'stock' => 'ftp://userPL099:QwqChVFh@ftp.axpol.com.pl/axpol_stocklist_pl.xml',
+	'marking' => 'ftp://userPL099:QwqChVFh@ftp.axpol.com.pl/axpol_print_data_PL.xml',
+	
+),
+array(
+	'shop' => 'AXPOL',
+	
+) );
+
 $SHOP['par'] = new PAR( array(
 	'products' => 'http://biuro@merkuriusz.pl:merkuriusz345@www.par.com.pl/api/products',
 	'stock' => 'http://biuro@merkuriusz.pl:merkuriusz345@www.par.com.pl/api/stocks',
@@ -47,11 +60,14 @@ array(
 	'shop' => 'PAR',
 	
 ) );
-$SHOP['anda'] = new ANDA( array(
-	'products' => 'http://andapresent.hu/admin/system/anda_xml_export2.php?&orszag_id=6&nyelv_id=7&password=92ba3632c8c22ebd65fbce872b317875',
+
+$SHOP['midoceanbrands'] = new MIDOCEANBRANDS( array(
+	'products' => 'ftp://lucyna:80838286@transfer.midoceanbrands.com/prodinfo_PL.xml',
+	'stock' => 'ftp://lucyna:80838286@transfer.midoceanbrands.com/stock.xml',
+	
 ),
 array(
-	'shop' => 'ANDA',
+	'shop' => 'MIDOCEANBRANDS',
 	
 ) );
 
